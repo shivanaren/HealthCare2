8f44fc82792316ccfdd6d0d34e60f256f26eab50
shivudu.kommu <shivudu.kommu@infosys.com>
2024-06-11 14:00:10 +0530

    shiva's commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,22 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { CoachesComponent } from './signup/coaches/coaches.component';
+import { SignupComponent } from './signup/signup.component';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
-  {
-    path:'home',
-    component:HomeComponent
-  },
-  {
-    path:'signup/coaches',
-    component:CoachesComponent
-  },
-  {
-    path:'',
-    redirectTo:'home',
-    pathMatch:'full',
-  }
+  {path:'home',component:HomeComponent},
+  {path:'', redirectTo:'home', pathMatch:'full'},
+  {path: 'signup/coaches', component: SignupComponent,data:{type:'coaches'}},
+  {path: 'signup/users', component: SignupComponent, data:{type:'users'}},
+  {path: 'login/coaches', component: LoginComponent }
 ];
 
 @NgModule({
